9dfe63dc9e48e0697607391682412717f078a86d
Seya <31957516+sglkc@users.noreply.github.com>
2025-01-02 16:57:48 +0700

    feat(vite): set local backend on command

--- src/vite/plugin.ts
+++ src/vite/plugin.ts
@@ -1,7 +1,6 @@
 import { ViteUserConfig } from 'astro';
-import type { CmsConfig } from 'decap-cms-core';
-import type { PreviewStyle } from '../types';
 import { generateVirtualConfigModule } from './virtual-module';
+import type { CmsConfig, PreviewStyle} from '../types';
 
 const virtualModuleId = 'virtual:astro-decap-cms/user-config';
 const resolvedVirtualModuleId = `\0${virtualModuleId}`;
@@ -9,7 +8,7 @@ const resolvedVirtualModuleId = `\0${virtualModuleId}`;
 type VitePlugin = ViteUserConfig['plugins'][number]
 
 interface AdminDashboardPluginOptions {
-	config: Omit<CmsConfig, 'load_config_file' | 'local_backend'>;
+	config: CmsConfig;
 	previewStyles: PreviewStyle[];
 	identityWidget: boolean;
 }
@@ -17,13 +16,20 @@ interface AdminDashboardPluginOptions {
 export default function AdminDashboardPlugin(
 	options: AdminDashboardPluginOptions
 ): VitePlugin {
+
+	// default options here
+	options.config.load_config_file = false
+
 	return {
 		name: 'vite-plugin-decap-cms-admin-dashboard',
 
+		// dynamic options here
+		config(_, { command }) {
+			options.config.local_backend = command === 'serve'
+		},
 		resolveId(id) {
 			if (id === virtualModuleId) return resolvedVirtualModuleId;
 		},
-
 		load(id) {
 			if (id === resolvedVirtualModuleId)
 				return generateVirtualConfigModule(options);
